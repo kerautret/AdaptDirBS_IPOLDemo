90daf087cd74407bb469ce7919be5d92fcdb6c1c
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2018-12-20 00:22:05 +0100

    draw line segments

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ class app(base_app):
                draw.ellipse((x - self.pensize, y - self.pensize, x + self.pensize + 1, y + self.pensize + 1), fill=254)
                outTips.write(str(x)+","+str(y)+",")
             if i % 2 == 0 and i != 0 :
-                draw.line(lastx, lasty, x, y, fill=128)
+                draw.line((lastx, lasty, x, y), fill=128)
             i = i + 1
             lastx = x
             lasty = y
